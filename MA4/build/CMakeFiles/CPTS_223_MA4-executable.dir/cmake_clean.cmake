335806a7a099123f54c452d704fe9a3627b4b53d
ssandland <78838051+ssandland@users.noreply.github.com>
2021-04-26 04:28:19 -0700

    Added MA5

---
+++ MA4/build/CMakeFiles/CPTS_223_MA4-executable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPTS_223_MA4-executable.dir/main.cpp.o"
+  "CPTS_223_MA4-executable"
+  "CPTS_223_MA4-executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPTS_223_MA4-executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
